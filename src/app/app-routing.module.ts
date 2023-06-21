f9f4b7b3cec15d673b5e87ab66ebaee45a61f4ae
GiPio <gcm.piantini@gmail.com>
2023-06-21 10:40:27 +0200

    create le rotte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormationComponent } from './formation/formation.component';
+import { PlayerComponent } from './player/player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'formation',
+    component: FormationComponent,
+  },
+  {
+    path: 'player/:id',
+    component: PlayerComponent,
+  },
+  { path: '**', redirectTo: '/contacts', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormationComponent } from './formation/formation.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormationComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
